3c019c76704e5122f3cc66644dc96e8f0056f14f
dongbin420 <121805002+dongbin420@users.noreply.github.com>
2023-02-03 19:16:13 +0900

    [level 0] Title: k의 개수, Time: 7.30 ms, Memory: 37 MB -BaekjoonHub

--- 프로그래머스/lv0/120887. k의 개수/k의 개수.js
+++ 프로그래머스/lv0/120887. k의 개수/k의 개수.js
@@ -1,11 +1,4 @@
 function solution(i, j, k) {
-    let answer = 0;
-    for(a=i; a<=j; a++) {
-        for(b=0; b<String(a).length; b++) {
-            if(String(a)[b] === String(k)) {
-                answer = answer+1
-            }
-        }
-    }
+    var answer = 0;
     return answer;
 }
