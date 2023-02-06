0b277a72879402178e3198fc721f52517ecb31e1
dongbin420 <121805002+dongbin420@users.noreply.github.com>
2023-02-07 00:35:18 +0900

    [level 0] Title: 한 번만 등장한 문자, Time: 2.41 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120896. 한 번만 등장한 문자/한 번만 등장한 문자.js
+++ 프로그래머스/lv0/120896. 한 번만 등장한 문자/한 번만 등장한 문자.js
@@ -1,30 +1,34 @@
 function solution(s) {
-    let arr = [];
-
-let arr1 = '';
+let arr = [];
 
 for (i = 0; i < s.length; i++) {
     arr.push(s[i])
-}
+}   // 빈 배열에 문자열을 알파벳 하나하나로 쪼개어 넣어줌. 
+
 
 let j = 0;
+console.log(arr.indexOf(s[0]) !== -1);
 while (true) {
-    if (arr.length !== 1) {
-        arr.splice(0, 1)
+
+    if (arr.indexOf(s[j]) !== -1) {
+        arr.splice(arr.indexOf(s[j]), 1)
         if (arr.includes(s[j])) {
             arr = arr.filter((element) => element !== s[j])
         } else {
-            break;
+            arr.push(s[j])
         }
-        j = j + 1;
-    } else {
+    }
+
+    j = j + 1;
+
+    if (j === s.length) {
         break;
     }
+}   // 배열의 길이가 1이면 무조건 한개만 남으므로, 1이 아닐때만을 체크해야한다.
+arr.sort();
+let answer = '';
+for (k = 0; k < arr.length; k++) {
+    answer = answer + arr[k]
 }
-    let answer = '';
-    arr.sort();
-    for(k=0; k<arr.length; k++) {
-        answer = answer+arr[k]
-    }
     return answer;
 }
