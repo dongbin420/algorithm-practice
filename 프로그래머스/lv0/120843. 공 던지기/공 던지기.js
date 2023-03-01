b56cfcf55cee8236670bf1927097762b6af1d9b9
dongbin420 <121805002+dongbin420@users.noreply.github.com>
2023-03-01 20:33:07 +0900

    [level 0] Title: 공 던지기, Time: 0.06 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv0/120843. 공 던지기/공 던지기.js
+++ 프로그래머스/lv0/120843. 공 던지기/공 던지기.js
@@ -1,10 +1,10 @@
-function solution(numbers, k) {
-    if(2*(k-1) > numbers.length-1) { // 2*(k-1) 은 number 가 무제한일때 답의 인덱스 번호
-        return numbers[2*(k-1) - numbers.length*(Math.floor(2*(k-1)/numbers.length))]; // 이렇게 하면 딱 한바퀴만 제거할텐데, 2*(k-1)/numbers.length
-    } else {
-     return numbers[2*(k-1)];
-    }
-}
+// function solution(numbers, k) {
+//     if(2*(k-1) > numbers.length-1) { // 2*(k-1) 은 number 가 무제한일때 답의 인덱스 번호
+//         return numbers[2*(k-1) - numbers.length*(Math.floor(2*(k-1)/numbers.length))]; // 이렇게 하면 딱 한바퀴만 제거할텐데, 2*(k-1)/numbers.length를 해서 몫 만 남긴 값을 numbers.length 에 곱해주면 적절히 빼줌.
+//     } else {
+//      return numbers[2*(k-1)];
+//     }
+// }
 
 // 입력: 친구들의 번호가 들어있는 정수 배열 numbers
 // 정수 k (이 k 번째로 공을 던지는 사람의 번호를 구해야함)
@@ -20,4 +20,18 @@ function solution(numbers, k) {
 
 // numbers의 length가 홀수면 0 2 1 0 2 1 (3) -> 0 2 4 1 3 0 2 4 1 3 (5) -> 0 2 4 6 1 3 5 0 2 4 6 1 3 5
 
-[1,2,3,4,5,6,7]
+// 채점 이후
+// 답이 맞았지만, 솔직히 이게 적절한 풀이는 아닌거같긴함 ㅎㅎ..
+
+function solution(numbers, k) {
+    let getBall = 1; // 인덱스값찾으려면 0 , 선수번호는 1
+// 첫 번째 선수가 공을 갖고있음 (첫번째 선수는 0번째 인덱스)
+for(let i = 0; i < k-1; i++) { // k가 5면, 0 1 2 3로 4번. 
+    getBall += 2; // 한번 던질때 마다 2씩 늘어나고, 따라서 getBall은 인덱스 값이 됨.
+    // 인덱스 값이 numbers의 최대 인덱스 값을 넘어가는 경우를 생각해야함
+    if(getBall > numbers.length) { // 인덱스값 찾으려면 numbers.length-1, 선수번호는 그냥 numbers.length
+        getBall -= numbers.length // 인덱스값이 최대인덱스를 넘어갈때마다 numbers의 길이만큼 빼면 그 다음차례 인덱스 값이 돰.
+    }
+}
+    return getBall;
+}
