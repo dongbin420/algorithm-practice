6b6b3e17fad2cfa7616b140f65d378abc86bc254
dongbin420 <121805002+dongbin420@users.noreply.github.com>
2023-03-28 00:43:43 +0900

    [level 0] Title: 문자열 계산하기, Time: 0.11 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv0/120902. 문자열 계산하기/문자열 계산하기.js
+++ 프로그래머스/lv0/120902. 문자열 계산하기/문자열 계산하기.js
@@ -24,3 +24,31 @@ newArr.forEach(function (el) {
 // 의사코드
 // 입력: 문자열로 된 수식 my_string (ex "3 + 5")
 // 출력: my_string을 계산한 값.
+
+// 추천많은 답
+// let my_string = "3 + 4 - 1";
+
+// const arr = my_string.split(' ');
+// console.log(arr)
+
+// while(arr.length > 1) arr.unshift(+arr.shift() + (arr.shift() === "+" ? 1 : -1) * arr.shift())
+
+// console.log(arr[0]);
+
+// 추천많은답2
+// function solution(my_string) {
+//     const stack = [];
+
+//     let sign = 1;
+//     for (const ch of my_string.split(" ")) {
+//         if (ch === "+") {
+//             sign = 1;
+//         } else if (ch === "-") {
+//             sign = -1;
+//         } else {
+//             stack.push(ch * sign);
+//         }
+//     }
+
+//     return stack.reduce((a,b) => a + b, 0);
+// }
