b33fcf8df22e4a7f995776e993d70cd896bb76a1
dongbin420 <121805002+dongbin420@users.noreply.github.com>
2023-04-01 20:56:28 +0900

    [level 1] Title: 음양 더하기, Time: 0.22 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/76501. 음양 더하기/음양 더하기.js
@@ -0,0 +1,10 @@
+function solution(absolutes, signs) {
+    let answer = 0;
+    for(let i = 0; i < signs.length; i++) {
+        if(signs[i] === false) {
+            absolutes[i] = -absolutes[i]
+        }
+        answer = answer + absolutes[i]
+    }
+    return answer;
+}
