968c4a42221e971cdb5b36c876e1de1320662076
dongbin420 <121805002+dongbin420@users.noreply.github.com>
2023-04-03 23:50:27 +0900

    [level 1] Title: 직사각형 별찍기, Time: 62.25 ms, Memory: 36.6 MB -BaekjoonHub

--- 프로그래머스/lv1/12969. 직사각형 별찍기/직사각형 별찍기.js
+++ 프로그래머스/lv1/12969. 직사각형 별찍기/직사각형 별찍기.js
@@ -2,12 +2,6 @@ process.stdin.setEncoding('utf8');
 process.stdin.on('data', data => {
     const n = data.split(" ");
     const a = Number(n[0]), b = Number(n[1]);
-    for(let i = 1; i <= b; i++) {
-        let newStr = "";
-        for(let j = 1; j <= a; j++) {
-            newStr = newStr + "*"
-        }
-        console.log(newStr);
-    }
+    console.log(a);
+    console.log(b);
 });
-
