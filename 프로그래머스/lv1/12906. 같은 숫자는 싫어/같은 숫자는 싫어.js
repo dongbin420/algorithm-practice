fda8e7974b34e05a795cb86f44e8d0f9c67ae381
dongbin420 <121805002+dongbin420@users.noreply.github.com>
2023-04-04 16:46:19 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 14.64 ms, Memory: 83.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.js
@@ -0,0 +1,10 @@
+function solution(arr)
+{
+    let newArr = [];
+    for(let i = 0; i < arr.length; i++) {
+        if((arr[i] === arr[i+1] && arr[i] !== arr[i-1]) || (arr[i] !== arr[i-1] && arr[i] !== arr[i+1])) {
+            newArr.push(arr[i]);
+        }
+    }
+    return newArr;
+}
