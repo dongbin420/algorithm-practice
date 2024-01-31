d3e20a0607856b1656ecb5c628c00ea0f6fe7a33
Dongbin Yoo <dongbin420@gmail.com>
2024-02-01 04:14:50 +0900

    [Silver II] Title: 랜선 자르기, Time: 228 ms, Memory: 14652 KB -BaekjoonHub

---
+++ 백준/Silver/1654. 랜선 자르기/랜선 자르기.js
@@ -0,0 +1,30 @@
+const fs = require('fs');
+const filePath = process.platform === 'linux' ? '/dev/stdin' : '../input.txt';
+let input = fs.readFileSync(filePath).toString().split('\n').map(line => line.replace(/\r/g, ''));
+
+const [ownCable, targetNum] = input.shift().split(' ');
+let low = 1; 
+let high = Math.max(...input);
+let result = 0;
+
+while (low <= high) {
+  let mid = Math.floor((low + high) / 2);
+  let cutCable = 0;
+
+  for (let i = 0; i < ownCable; i++) {
+    cutCable += Math.floor(input[i] / mid);
+  }
+
+  if (cutCable < targetNum) {
+    high = mid - 1;
+  } else if (cutCable === targetNum) {
+    result = mid;
+
+    break;
+  } else {
+    result = mid;
+    low = mid + 1;
+  }
+}
+
+console.log(result);
