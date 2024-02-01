85b5f050efaa6cb89286f436512c5c6ad9b03d0a
Dongbin Yoo <dongbin420@gmail.com>
2024-02-01 20:40:40 +0900

    [Bronze V] Title: 문자와 문자열, Time: 116 ms, Memory: 9328 KB -BaekjoonHub

---
+++ 백준/Bronze/27866. 문자와 문자열/문자와 문자열.js
@@ -0,0 +1,5 @@
+const fs = require('fs');
+const filePath = process.platform === 'linux' ? '/dev/stdin' : '../../input.txt';
+let input = fs.readFileSync(filePath).toString().split('\n').map(line => line.replace(/\r/g, ''));
+
+console.log(input[0][Number(input[1]) - 1]);
