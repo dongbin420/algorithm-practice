8e1aa6fb29c63c8e19a79a4a2e80011f0432e992
Dongbin Yoo <dongbin420@gmail.com>
2024-02-06 18:58:24 +0900

    [Bronze III] Title: 직각삼각형, Time: 120 ms, Memory: 9360 KB -BaekjoonHub

---
+++ 백준/Bronze/4153. 직각삼각형/직각삼각형.js
@@ -0,0 +1,21 @@
+const fs = require('fs');
+const filePath = process.platform === 'linux' ? '/dev/stdin' : '../../input.txt';
+let input = fs.readFileSync(filePath).toString().trim().split('\n').map(line => line.replace(/\r/g, '').split(' ').map(num => Number(num)));
+
+const answer = [];
+
+for (let i = 0; i < input.length; i++) {
+  if (input[i][0] === 0 && input[i][1] === 0 && input[i][2] === 0) {
+    break;
+  }
+
+  const sortedInput = input[i].sort((a, b) => a - b);
+  const [firstSide, secondSide, thirdSide] = sortedInput;
+
+  const isRight = (firstSide ** 2 + secondSide ** 2) === thirdSide ** 2 ? 'right' : 'wrong';
+  answer.push(isRight);
+}
+
+if (answer.length > 0) {
+  console.log(answer.join('\n'));
+}
