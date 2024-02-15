96fedf2a7d590586d1754918dafd58625c78956b
Dongbin Yoo <dongbin420@gmail.com>
2024-02-15 19:43:51 +0900

    [Silver III] Title: 1로 만들기, Time: 300 ms, Memory: 17440 KB -BaekjoonHub

---
+++ 백준/Silver III/1463. 1로 만들기/1로 만들기.js
@@ -0,0 +1,20 @@
+const fs = require('fs');
+const filePath = process.platform === 'linux' ? '/dev/stdin' : '../../input.txt';
+let input = fs.readFileSync(filePath).toString().trim();
+
+let inputNum = Number(input);
+
+function getMinOperations(num) {
+  let dp = new Array(num + 1).fill(0);
+
+  for (let i = 2; i < dp.length; i++) {
+    dp[i] = dp[i - 1] + 1;
+
+    if (i % 2 === 0) dp[i] = Math.min(dp[i], dp[i / 2] + 1);
+    if (i % 3 === 0) dp[i] = Math.min(dp[i], dp[i / 3] + 1);
+  }
+
+  return dp[num];
+}
+
+console.log(getMinOperations(inputNum));
