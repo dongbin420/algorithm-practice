9cc35a0b80740190a2971be489f955c39a141ca0
Dongbin Yoo <dongbin420@gmail.com>
2024-02-20 19:32:00 +0900

    [Silver II] Title: DFS와 BFS, Time: 204 ms, Memory: 14724 KB -BaekjoonHub

--- 백준/Silver/1260. DFS와 BFS/DFS와 BFS.js
+++ 백준/Silver/1260. DFS와 BFS/DFS와 BFS.js
@@ -19,49 +19,51 @@ const dfsResult = [];
 const bfsResult = [];
 
 // 재귀
-const dfs = (vertex) => {
-  dfsVisited[vertex] = true;
-  dfsResult.push(vertex);
-
-  for (let i = 0; i < graph[vertex].length; i++) {
-    const adj = graph[vertex][i];
-
-    if (!dfsVisited[adj]) {
-      dfs(adj);
-    }
-  }
-}
-
-//스택
 // const dfs = (vertex) => {
-//   const stack = [vertex];
 //   dfsVisited[vertex] = true;
+//   dfsResult.push(vertex);
 
-//   while (stack.length !== 0) {
-//     const vertex = stack.pop();
-//     dfsResult.push(vertex);
+//   for (let i = 0; i < graph[vertex].length; i++) {
+//     const adj = graph[vertex][i];
 
-//     for (let i = graph[vertex].length - 1; i >= 0; i--) {
-//       const adj = graph[vertex][i];
-
-//       if (!dfsVisited[adj]) {
-//         dfsVisited[adj] = true;
-//         stack.push(adj);
-//       }
+//     if (!dfsVisited[adj]) {
+//       dfs(adj);
 //     }
 //   }
 // }
+
+//스택
+const dfs = (vertex) => {
+  const stack = [vertex];
+
+  while (stack.length !== 0) {
+    const vtx = stack.pop();
+
+    if (!dfsVisited[vtx]) {
+      dfsVisited[vtx] = true;
+      dfsResult.push(vtx);
+
+      for (let i = graph[vtx].length - 1; i >= 0; i--) {
+        const adj = graph[vtx][i];
+
+        if (!dfsVisited[adj]) {
+          stack.push(adj);
+        }
+      }
+    }
+  }
+}
 
 const bfs = (vertex) => {
   const queue = [vertex];
   bfsVisited[vertex] = true;
 
   while (queue.length !== 0) {
-    const vertex = queue.shift();
-    bfsResult.push(vertex);
+    const vtx = queue.shift();
+    bfsResult.push(vtx);
 
-    for (let i = 0; i < graph[vertex].length; i++) {
-      const adj = graph[vertex][i];
+    for (let i = 0; i < graph[vtx].length; i++) {
+      const adj = graph[vtx][i];
 
       if (!bfsVisited[adj]) {
         bfsVisited[adj] = true;
