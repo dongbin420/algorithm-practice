7603273fe1a4f4e07b5d0655a3068a8586fae5bc
Dongbin Yoo <dongbin420@gmail.com>
2024-02-22 20:00:51 +0900

    [Silver I] Title: 미로 탐색, Time: 192 ms, Memory: 11788 KB -BaekjoonHub

---
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.js
@@ -0,0 +1,36 @@
+const fs = require('fs');
+const filePath = process.platform === 'linux' ? '/dev/stdin' : '../../../input.txt';
+let input = fs.readFileSync(filePath).toString().trim().split('\n').map(line => line.replace(/\r/g, ''));
+
+const [N, M] = input.shift().split(' ').map(Number);
+
+const maze = input.map(row => row.split('').map(Number));
+const visited = Array.from(Array(N), () => Array(M).fill(false));
+
+const dx = [-1, 1, 0, 0];
+const dy = [0, 0, -1, 1];
+
+function bfs() {
+  const queue = [];
+  queue.push([0, 0]);
+  visited[0][0] = true;
+
+  while (queue.length > 0) {
+    const [x, y] = queue.shift();
+
+    for (let i = 0; i < 4; i++) {
+      const nx = x + dx[i];
+      const ny = y + dy[i];
+
+      if (nx >= 0 && nx < N && ny >= 0 && ny < M && maze[nx][ny] === 1 && !visited[nx][ny]) {
+        visited[nx][ny] = true;
+        maze[nx][ny] = maze[x][y] + 1;
+        queue.push([nx, ny]);
+      }
+    }
+  }
+}
+
+bfs();
+
+console.log(maze[N - 1][M - 1]);
