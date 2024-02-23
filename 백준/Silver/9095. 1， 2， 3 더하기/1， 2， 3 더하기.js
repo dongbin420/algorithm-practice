3a0f6216bad44ce2cbcd9b1ab7fc38a4faa60724
Dongbin Yoo <dongbin420@gmail.com>
2024-02-23 19:29:04 +0900

    [Silver III] Title: 1, 2, 3 더하기, Time: 120 ms, Memory: 9324 KB -BaekjoonHub

--- 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.js
+++ 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.js
@@ -2,18 +2,21 @@ const fs = require('fs');
 const filePath = process.platform === 'linux' ? '/dev/stdin' : '../../input.txt';
 let input = fs.readFileSync(filePath).toString().trim().split('\n').map(line => Number(line.replace(/\r/g, '')));
 
-const T = Number(input.shift());
-const dp = Array(12).fill(0); // 범위에 있는 모든 정수에 대하여 합을 만드는 방법의 수에 대한 배열
+input.shift();
 const answer = [];
 
-dp[1] = 1;
-dp[2] = 2;
-dp[3] = 4;
+input.forEach((input) => {
+  const dp = [];
 
-for (let i = 4; i <= 11; i++) {
-  dp[i] = (dp[i - 1] + dp[i - 2] + dp[i - 3]);
-}
+  dp[1] = 1;
+  dp[2] = 2;
+  dp[3] = 4;
 
-input.forEach((input) => answer.push(dp[input]))
+  for(let i = 4; i <= input; i++) {
+    dp[i] = dp[i - 1] + dp[i - 2] + dp[i - 3];
+  }
+
+  answer.push(dp[input]);
+})
 
 console.log(answer.join('\n'));
