4ae3677eb81466fa31a48f0a37b3bdc9f80199be
Dongbin Yoo <dongbin420@gmail.com>
2024-03-15 17:33:59 +0900

    [Silver III] Title: 파도반 수열, Time: 120 ms, Memory: 9600 KB -BaekjoonHub

--- 백준/Silver/9461. 파도반 수열/파도반 수열.js
+++ 백준/Silver/9461. 파도반 수열/파도반 수열.js
@@ -4,11 +4,19 @@ let input = fs.readFileSync(filePath).toString().trim().split('\n').map(Number);
 
 const T = input.shift();
 const maxInput = Math.max(...input);
+const result = [];
 
-const P = [0, 1, 1, 1, 2, 2];
+const P = [0];
+P[1] = 1;
+P[2] = 1;
+P[3] = 1;
+P[4] = 2;
+P[5] = 2;
 
 for (let i = 6; i <= maxInput; i++) {
   P[i] = P[i - 1] + P[i - 5];
 }
 
-input.forEach(N => console.log(P[N]));
+input.forEach(N => result.push(P[N]));
+
+console.log(result.join('\n'));
