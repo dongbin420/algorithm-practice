a52c1db2eed5c50229481a09bfae4101a71dceaf
Dongbin Yoo <dongbin420@gmail.com>
2024-03-15 17:26:31 +0900

    [Silver III] Title: 파도반 수열, Time: 136 ms, Memory: 9828 KB -BaekjoonHub

--- 백준/Silver/9461. 파도반 수열/파도반 수열.js
+++ 백준/Silver/9461. 파도반 수열/파도반 수열.js
@@ -1,15 +1,9 @@
 const fs = require('fs');
 const filePath = process.platform === 'linux' ? '/dev/stdin' : '../../input.txt';
-let input = fs.readFileSync(filePath).toString().trim().split('\n').map(line => Number(line.replace(/\r/g, '')));
+let input = fs.readFileSync(filePath).toString().trim().split('\n').map(Number);
 
 const T = input.shift();
-let max = 0;
-
-input.forEach((N) => {
-  if (max < N) {
-    max = N;
-  }
-})
+const maxInput = Math.max(...input);
 
 const P = [0];
 P[1] = 1;
@@ -18,7 +12,7 @@ P[3] = 1;
 P[4] = 2;
 P[5] = 2;
 
-for (let i = 6; i <= max; i++) {
+for (let i = 6; i <= maxInput; i++) {
   P[i] = P[i - 1] + P[i - 5];
 }
 
