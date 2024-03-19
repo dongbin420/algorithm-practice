810edb1af9e4c05129ad9c46b29b716684eeab0b
Dongbin Yoo <dongbin420@gmail.com>
2024-03-19 17:12:45 +0900

    [Silver II] Title: 나무 자르기, Time: 664 ms, Memory: 134892 KB -BaekjoonHub

--- 백준/Silver/2805. 나무 자르기/나무 자르기.js
+++ 백준/Silver/2805. 나무 자르기/나무 자르기.js
@@ -6,21 +6,21 @@ const [numOfTrees, goal] = input[0].split(' ').map(Number);
 const trees = input[1].split(' ').map(Number);
 
 let low = 0;
-let high = Math.max(...trees) - 1;
+let high = Math.max(...trees);
 let answer = 0;
 
 while (low <= high) {
   let mid = Math.floor((low + high) / 2);
   let total = 0;
 
-  trees.forEach((tree) => {
-    if (tree > mid) {
-      total += tree - mid;
+  for (let i = 0; i < numOfTrees; i++) {
+    if (trees[i] > mid) {
+      total += trees[i] - mid;
     }
-  })
+  }
 
   if (total >= goal) {
-    answer = Math.max(answer, mid);
+    answer = mid;
     low = mid + 1;
   } else {
     high = mid - 1;
