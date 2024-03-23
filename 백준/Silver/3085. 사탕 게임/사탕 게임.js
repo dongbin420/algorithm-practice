9b6da8bbd67948286054ecbdaf781a2ae18430a7
Dongbin Yoo <dongbin420@gmail.com>
2024-03-24 01:07:53 +0900

    [Silver II] Title: 사탕 게임, Time: 364 ms, Memory: 10344 KB -BaekjoonHub

--- 백준/Silver/3085. 사탕 게임/사탕 게임.js
+++ 백준/Silver/3085. 사탕 게임/사탕 게임.js
@@ -4,28 +4,28 @@ let input = fs.readFileSync(filePath).toString().trim().split('\n').map(line => 
 
 const N = Number(input.shift());
 input = input.map(line => line.split(''));
-let answer = 0;
+let answer = -Infinity;
 
 function getMaxCandy(arr) {
   let maxCandyCnt = 0;
 
   for (let i = 0; i < N; i++) {
-    let testNumRow = 1;
-    let testNumCol = 1;
+    let rowCnt = 1;
+    let colCnt = 1;
 
     for (let j = 0; j < N; j++) {
       if (j < N - 1 && arr[i][j] === arr[i][j + 1]) {
-        testNumRow++;
+        rowCnt++;
       } else {
-        maxCandyCnt = Math.max(maxCandyCnt, testNumRow);
-        testNumRow = 1;
+        maxCandyCnt = Math.max(maxCandyCnt, rowCnt);
+        rowCnt = 1;
       }
 
       if (j < N - 1 && arr[j][i] === arr[j + 1][i]) {
-        testNumCol++;
+        colCnt++;
       } else {
-        maxCandyCnt = Math.max(maxCandyCnt, testNumCol);
-        testNumCol = 1;
+        maxCandyCnt = Math.max(maxCandyCnt, colCnt);
+        colCnt = 1;
       }
     }
   }
@@ -36,15 +36,23 @@ function getMaxCandy(arr) {
 for (let i = 0; i < N; i++) {
   for (let j = 0; j < N; j++) {
     if (j < N - 1 && input[i][j] !== input[i][j + 1]) {
-      [input[i][j], input[i][j + 1]] = [input[i][j + 1], input[i][j]];
-      answer = Math.max(getMaxCandy(input), answer);
-      [input[i][j], input[i][j + 1]] = [input[i][j + 1], input[i][j]];
+      const first = input[i][j];
+      const second = input[i][j + 1];
+      input[i][j] = second;
+      input[i][j + 1] = first;
+      answer = Math.max(answer, getMaxCandy(input));
+      input[i][j] = first;
+      input[i][j + 1] = second;
     }
 
-    if (i < N - 1 && input[i][j] !== input[i + 1][j]) {
-      [input[i][j], input[i + 1][j]] = [input[i + 1][j], input[i][j]];
-      answer = Math.max(getMaxCandy(input), answer);
-      [input[i][j], input[i + 1][j]] = [input[i + 1][j], input[i][j]];
+    if (j < N - 1 && input[j][i] !== input[j + 1][i]) {
+      const first = input[j][i];
+      const second = input[j + 1][i];
+      input[j][i] = second;
+      input[j + 1][i] = first;
+      answer = Math.max(answer, getMaxCandy(input));
+      input[j][i] = first;
+      input[j + 1][i] = second;
     }
   }
 }
