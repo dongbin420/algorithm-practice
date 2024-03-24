2f45a46da633eb8d05168319f52fb47260bdcd0d
Dongbin Yoo <dongbin420@gmail.com>
2024-03-24 22:00:49 +0900

    [Silver II] Title: 사탕 게임, Time: 184 ms, Memory: 10360 KB -BaekjoonHub

--- 백준/Silver/3085. 사탕 게임/사탕 게임.js
+++ 백준/Silver/3085. 사탕 게임/사탕 게임.js
@@ -1,60 +1,72 @@
 const fs = require('fs');
-const filePath = process.platform === 'linux' ? '/dev/stdin' : '../../input.txt';
+const filePath = process.platform === 'linux' ? '/dev/stdin' : './input.txt';
 let input = fs.readFileSync(filePath).toString().trim().split('\n').map(line => line.replace(/\r/g, ''));
 
 const N = Number(input.shift());
 input = input.map(line => line.split(''));
 let answer = -Infinity;
 
-function getMaxCandy(arr) {
-  let maxCandyCnt = 0;
-
-  for (let i = 0; i < N; i++) {
-    let rowCnt = 1;
-    let colCnt = 1;
-
-    for (let j = 0; j < N; j++) {
-      if (j < N - 1 && arr[i][j] === arr[i][j + 1]) {
-        rowCnt++;
-      } else {
-        maxCandyCnt = Math.max(maxCandyCnt, rowCnt);
-        rowCnt = 1;
-      }
-
-      if (j < N - 1 && arr[j][i] === arr[j + 1][i]) {
-        colCnt++;
-      } else {
-        maxCandyCnt = Math.max(maxCandyCnt, colCnt);
-        colCnt = 1;
-      }
-    }
-  }
-
-  return maxCandyCnt;
-}
-
 for (let i = 0; i < N; i++) {
   for (let j = 0; j < N; j++) {
-    if (j < N - 1 && input[i][j] !== input[i][j + 1]) {
+    if (j < N - 1) {
       const first = input[i][j];
       const second = input[i][j + 1];
       input[i][j] = second;
       input[i][j + 1] = first;
-      answer = Math.max(answer, getMaxCandy(input));
+      answer = Math.max(answer, getMaxCandies(i, j), getMaxCandies(i, j + 1));
       input[i][j] = first;
       input[i][j + 1] = second;
     }
 
-    if (j < N - 1 && input[j][i] !== input[j + 1][i]) {
+    if (j < N - 1) {
       const first = input[j][i];
       const second = input[j + 1][i];
       input[j][i] = second;
       input[j + 1][i] = first;
-      answer = Math.max(answer, getMaxCandy(input));
+      answer = Math.max(answer, getMaxCandies(j, i), getMaxCandies(j + 1, i));
       input[j][i] = first;
       input[j + 1][i] = second;
     }
   }
+}
+
+function getMaxCandies(x, y) {
+  let maxCandiesCol = 1;
+  let maxCandiesRow = 1;
+
+  for (let i = y - 1; i >= 0; i--) {
+    if (input[x][y] === input[x][i]) {
+      maxCandiesCol++;
+    } else {
+      break;
+    }
+  }
+
+  for (let i = y + 1; i < N; i++) {
+    if (input[x][y] === input[x][i]) {
+      maxCandiesCol++;
+    } else {
+      break;
+    }
+  }
+
+  for (let i = x - 1; i >= 0; i--) {
+    if (input[x][y] === input[i][y]) {
+      maxCandiesRow++;
+    } else {
+      break;
+    }
+  }
+
+  for (let i = x + 1; i < N; i++) {
+    if (input[x][y] === input[i][y]) {
+      maxCandiesRow++;
+    } else {
+      break;
+    }
+  }
+
+  return Math.max(maxCandiesRow, maxCandiesCol);
 }
 
 console.log(answer);
