7f77b37eb4b2fab9a50535e58e9e8ee8f8cd096d
Dongbin Yoo <dongbin420@gmail.com>
2024-03-28 16:26:32 +0900

    [Silver IV] Title: 듣보잡, Time: 244 ms, Memory: 31000 KB -BaekjoonHub

--- 백준/Silver/1764. 듣보잡/듣보잡.js
+++ 백준/Silver/1764. 듣보잡/듣보잡.js
@@ -3,9 +3,8 @@ const filePath = process.platform === 'linux' ? '/dev/stdin' : './input.txt';
 let input = fs.readFileSync(filePath).toString().trim().split('\n').map(line => line.replace(/\r/g, ''));
 
 const [N, M] = input.shift().split(' ').map(Number);
-const heard = new Set(input.slice(0, N));
-const seen = input.slice(N);
-const picks = seen.filter(person => heard.has(person)).sort();
 
-console.log(picks.length);
-console.log(picks.join('\n'));
+const heard = new Set(input.splice(0, N));
+const picks = input.filter(person => heard.has(person)).sort();
+
+console.log(picks.length + '\n' + picks.join('\n'));
