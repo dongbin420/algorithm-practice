39339d8e6305a6dbe78614830d5365eaa1336f47
Dongbin Yoo <dongbin420@gmail.com>
2024-04-02 19:12:29 +0900

    [Silver III] Title: 구간 합 구하기 4, Time: 344 ms, Memory: 48804 KB -BaekjoonHub

--- 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.js
+++ 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.js
@@ -1,22 +1,19 @@
 const fs = require('fs');
-const filePath = process.platform === 'linux' ? '/dev/stdin' : './input.txt';
-let input = fs.readFileSync(filePath).toString().trim().split('\n').map(line => line.replace(/\r/g, ''));
+const input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
+const [N, M] = input[0].split(' ').map(Number);
+const numList = input[1].split(' ').map(Number);
+const ranges = input.slice(2);
 
-const [N, M] = input.shift().split(' ').map(Number);
-const nums = input.shift().split(' ').map(Number);
-input = input.map(section => section.split(' ').map(Number));
-
+const sumList = [0, numList[0]];
 const answer = [];
-const prefixSum = [0];
 
-for (let i = 0; i < N; i++) {
-  prefixSum.push(prefixSum[i] + nums[i]);
+for (let i = 1; i < N; i++) {
+    sumList.push(numList[i] + sumList[i]);
 }
 
-for (let j = 0; j < M; j++) {
-  const [from, to] = input[j];
-  const sum = prefixSum[to] - prefixSum[from - 1];
-  answer.push(sum);
+for (let i = 0; i < M; i++) {
+    const range = ranges[i].split(' ').map(Number);
+    answer.push(sumList[range[1]] - sumList[range[0] - 1]);
 }
 
 console.log(answer.join('\n'));
