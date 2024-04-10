05f8391d92cbda760299ec29e885ac13532b5c79
Dongbin Yoo <dongbin420@gmail.com>
2024-04-10 16:32:28 +0900

    [Silver IV] Title: 나는야 포켓몬 마스터 이다솜, Time: 392 ms, Memory: 60580 KB -BaekjoonHub

--- 백준/Silver/1620. 나는야 포켓몬 마스터 이다솜/나는야 포켓몬 마스터 이다솜.js
+++ 백준/Silver/1620. 나는야 포켓몬 마스터 이다솜/나는야 포켓몬 마스터 이다솜.js
@@ -1,23 +1,20 @@
-// 백준 1620
 const fs = require('fs');
-const filePath = process.platform === 'linux' ? '/dev/stdin' : './input.txt';
+const filePath = process.platform === 'linux' ? '/dev/stdin' : '../../input.txt';
 let input = fs.readFileSync(filePath).toString().trim().split('\n').map(line => line.replace(/\r/g, ''));
 
 const [N, M] = input.shift().split(' ').map(Number);
 const pocketmons = input.slice(0, N);
 const questions = input.slice(N);
 const book = new Map();
-const reverseBook = new Map();
 const answer = [];
 
 for (let i = 0; i < N; i++) {
   book.set(pocketmons[i], i + 1);
-  reverseBook.set(i + 1, pocketmons[i]);
 }
 
 for (let j = 0; j < M; j++) {
   if (!isNaN(questions[j])) {
-    answer.push(reverseBook.get(Number(questions[j])));
+    answer.push(pocketmons[Number(questions[j]) - 1]);
   } else {
     answer.push(book.get(questions[j]));
   }
