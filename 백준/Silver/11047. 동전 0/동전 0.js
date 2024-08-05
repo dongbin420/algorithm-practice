d4b325e98dc9f4e25cff19a81c49a2455a1b565d
Dongbin Yoo <dongbin420@gmail.com>
2024-08-05 15:14:32 +0900

    [Silver IV] Title: 동전 0, Time: 220 ms, Memory: 9616 KB -BaekjoonHub

--- 백준/Silver/11047. 동전 0/동전 0.js
+++ 백준/Silver/11047. 동전 0/동전 0.js
@@ -1,24 +1,18 @@
-const fs = require("fs");
-const filePath = process.platform === "linux" ? "/dev/stdin" : "../input.txt";
-let input = fs.readFileSync(filePath).toString().split("\n");
+const fs = require('fs');
+const filePath = process.platform === 'linux' ? '/dev/stdin' : '../../input.txt';
+let input = fs.readFileSync(filePath).toString().split('\n');
 
-firstLineInput = input[0].split(' ');
+let [N, K] = input.shift().split(' ').map(Number);
+const editedInput = input.map(Number).sort((a, b) => b - a);
+let cnt = 0;
 
-let coinUnitNum = +firstLineInput[0];
-let goalMoney = +firstLineInput[1];
-let coinUnitArr = [];
-let answer = 0;
+for (let i = 0; i < N; i++) {
+  while (editedInput[i] <= K) {
+    K = K - editedInput[i];
+    cnt++;
+  }
 
-
-for(let i = 1; i < coinUnitNum + 1; i++) {
-    coinUnitArr.push(+input[i]);
+  if (K === 0) break;
 }
 
-for(let j = coinUnitArr.length - 1; j >= 0; j--) {
-    while(coinUnitArr[j] <= goalMoney) {
-        goalMoney -= coinUnitArr[j];
-        answer++;
-    }
-}
-
-console.log(answer);
+console.log(cnt);
