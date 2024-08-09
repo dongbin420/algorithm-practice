81be6c8778a3987a6907112c3d19fcd605c34f8f
Dongbin Yoo <dongbin420@gmail.com>
2024-08-09 17:06:20 +0900

    [Silver III] Title: 프린터 큐, Time: 160 ms, Memory: 11600 KB -BaekjoonHub

--- 백준/Silver/1966. 프린터 큐/프린터 큐.js
+++ 백준/Silver/1966. 프린터 큐/프린터 큐.js
@@ -1,38 +1,36 @@
 const fs = require('fs');
 const filePath = process.platform === 'linux' ? '/dev/stdin' : '../../input.txt';
-let input = fs.readFileSync(filePath).toString().trim().split('\n').map(line => line.replace(/\r/g, ''));
+let input = fs
+  .readFileSync(filePath)
+  .toString()
+  .trim()
+  .split('\n')
+  .map((line) => line.replace(/\r/g, ''));
 
-const testCase = Number(input.shift());
-const answer = [];
+const T = Number(input.shift());
+let answer = '';
 
-for (let i = 0; i < testCase; i++) {
-  let [total, targetIdx] = input.shift().split(' ').map(Number);
-  const priorities = input.shift().split(' ').map(Number);
-  let cnt = 0;
+for (let i = 0; i < input.length; i = i + 2) {
+  let cnt = 1;
+  const [N, target] = input[i].split(' ').map(Number);
+  const imporArr = input[i + 1].split(' ').map(Number);
+  const stack = imporArr.map((impor, idx) => [impor, idx]);
 
   while (true) {
-    const front = priorities.shift();
-
-    if (priorities.some(priority => priority > front)) {
-      priorities.push(front);
-
-      if (targetIdx === 0) {
-        targetIdx = priorities.length - 1;
-      } else {
-        targetIdx--;
-      }
-    } else {
-      cnt++;
-
-      if (targetIdx === 0) {
-        answer.push(cnt);
+    if (stack[0][0] === Math.max(...stack.map((arr) => arr[0]))) {
+      if (stack[0][1] === target) {
+        answer += `${cnt}\n`;
+        cnt = 1;
 
         break;
       } else {
-        targetIdx--;
+        stack.shift();
+        cnt++;
       }
+    } else {
+      stack.push(stack.shift());
     }
   }
 }
 
-console.log(answer.join('\n'));
+console.log(answer.trim());
