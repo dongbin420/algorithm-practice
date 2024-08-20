9ea5b4c9ba8081ca5961a4a42422f3d409c28ca0
Dongbin Yoo <dongbin420@gmail.com>
2024-08-20 16:29:18 +0900

    [Silver V] Title: 팩토리얼 0의 개수, Time: 96 ms, Memory: 9592 KB -BaekjoonHub

--- 백준/Silver/1676. 팩토리얼 0의 개수/팩토리얼 0의 개수.js
+++ 백준/Silver/1676. 팩토리얼 0의 개수/팩토리얼 0의 개수.js
@@ -2,20 +2,18 @@ const fs = require('fs');
 const filePath = process.platform === 'linux' ? '/dev/stdin' : '../../input.txt';
 let input = BigInt(fs.readFileSync(filePath).toString());
 
-let factorial = 1n;
-
-for (let i = 2n; i <= input; i++) {
-  factorial *= i;
-}
-
-const editedFactorial = String(factorial);
+let factorialResult = 1n;
 let answer = 0;
 
-for (let j = editedFactorial.length - 1; j >= 0; j--) {
-  if (editedFactorial[j] === '0') {
+for (let i = 2n; i <= input; i++) {
+  factorialResult *= i;
+}
+
+for (let j = String(factorialResult).length - 1; j >= 0; j--) {
+  if (String(factorialResult)[j] === '0') {
     answer++;
   } else {
-    break; 
+    break;
   }
 }
 
