2cad7a7f444fe6ab661d59cbc3163fe2e47a6f07
Dongbin Yoo <dongbin420@gmail.com>
2024-08-22 16:58:48 +0900

    [Silver IV] Title: 수 찾기, Time: 296 ms, Memory: 37140 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.js
+++ 백준/Silver/1920. 수 찾기/수 찾기.js
@@ -1,37 +1,44 @@
 const fs = require('fs');
 const filePath = process.platform === 'linux' ? '/dev/stdin' : '../../input.txt';
-let input = fs.readFileSync(filePath).toString().split('\n').map(line => line.replace(/\r/g, ''));
+let input = fs
+  .readFileSync(filePath)
+  .toString()
+  .split('\n')
+  .map((line) => line.replace(/\r/g, ''));
 
 const N = Number(input[0]);
-const baseArr = input[1].split(' ').map(num => Number(num)).sort((a, b) => a - b);
+const inputNums = input[1]
+  .split(' ')
+  .map(Number)
+  .sort((a, b) => a - b);
 const M = Number(input[2]);
-const checkNums = input[3].split(' ').map(num => Number(num));
+const targetNums = input[3].split(' ').map(Number);
 
-const answer = [];
+let answer = '';
 
 for (let i = 0; i < M; i++) {
   let low = 0;
-  let high = baseArr.length - 1;
-  let found = false;
+  let high = N - 1;
+  let isExist = false;
 
   while (low <= high) {
     let mid = Math.floor((low + high) / 2);
-    
-    if (checkNums[i] < baseArr[mid]) {
+
+    if (targetNums[i] < inputNums[mid]) {
       high = mid - 1;
-    } else if (checkNums[i] > baseArr[mid]) {
+    } else if (targetNums[i] > inputNums[mid]) {
       low = mid + 1;
     } else {
-      answer.push('1');
-      found = true;
+      answer += '1\n';
+      isExist = true;
 
       break;
-    } 
+    }
   }
 
-  if (!found) {
-    answer.push('0');
+  if (!isExist) {
+    answer += '0\n';
   }
 }
 
-console.log(answer.join('\n'));
+console.log(answer.trim());
