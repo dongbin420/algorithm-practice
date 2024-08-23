18df6704989114d507dfe52c7c6e29da1d9f3bc4
Dongbin Yoo <dongbin420@gmail.com>
2024-08-24 04:10:41 +0900

    [Bronze II] Title: 소수 찾기, Time: 92 ms, Memory: 9352 KB -BaekjoonHub

--- 백준/Bronze/1978. 소수 찾기/소수 찾기.js
+++ 백준/Bronze/1978. 소수 찾기/소수 찾기.js
@@ -1,29 +1,28 @@
 const fs = require('fs');
 const filePath = process.platform === 'linux' ? '/dev/stdin' : '../../input.txt';
-let input = fs.readFileSync(filePath).toString().split('\n').map(line => line.replace(/\r/g, ''));
+let input = fs
+  .readFileSync(filePath)
+  .toString()
+  .split('\n')
+  .map((line) => line.replace(/\r/g, ''));
 
-const N = input.shift();
-const editedInput = input[0].split(' ').map(num => Number(num));
-let answer = 0;
+const N = Number(input.shift());
+const numInput = input[0].split(' ').map(Number);
+const primeNums = Array(Math.max(...numInput) + 1).fill(true);
+primeNums[0] = primeNums[1] = false;
+let cnt = 0;
 
-for (let i = 0; i < N; i++) {
-  let isTargetNum = true;
-
-  if (editedInput[i] === 1) {
-    isTargetNum = false;
-  } else {
-    for (let j = 2; j < editedInput[i]; j++) {
-      if (editedInput[i] % j === 0) {
-        isTargetNum = false;
-
-        break;
-      }
-    }
+// max의 제곱근 까지만 소수를 검사하면 되는 이유는, max보다 작은 수의 제곱이 max보다 크면 이 max를 커버할 수 있기 때문(?)
+for (let i = 2; i <= Math.sqrt(Math.max(...numInput)); i++) {
+  for (let j = i * i; j <= Math.max(...numInput); j += i) {
+    primeNums[j] = false;
   }
-
-  if (isTargetNum) {
-    answer += 1;
-  } 
 }
 
-console.log(answer);
+for (let num of numInput) {
+  if (primeNums[num]) {
+    cnt++;
+  }
+}
+
+console.log(cnt);
