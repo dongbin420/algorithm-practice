46d4c46368bf866d547c1b5a53ec61c8e0402b8f
Dongbin Yoo <dongbin420@gmail.com>
2024-08-28 20:07:04 +0900

    [Bronze II] Title: 블랙잭, Time: 148 ms, Memory: 9604 KB -BaekjoonHub

--- 백준/Bronze/2798. 블랙잭/블랙잭.js
+++ 백준/Bronze/2798. 블랙잭/블랙잭.js
@@ -1,21 +1,30 @@
 const fs = require('fs');
 const filePath = process.platform === 'linux' ? '/dev/stdin' : '../../input.txt';
-let input = fs.readFileSync(filePath).toString().split('\n').map(line => line.replace(/\r/g, ''));
+let input = fs
+  .readFileSync(filePath)
+  .toString()
+  .split('\n')
+  .map((line) => line.replace(/\r/g, ''));
 
-const [N, M] = input.shift().split(' ').map(num => Number(num));
-const editedInput = input[0].split(' ').map(num => Number(num));
-const maxSumArr = [];
+const [N, M] = input.shift().split(' ').map(Number);
+const cards = input[0].split(' ').map(Number);
+let sum = 0;
 
 for (let i = 0; i < N; i++) {
+  let firstNum = cards[i];
+
   for (let j = i + 1; j < N; j++) {
+    let secondNum = cards[j];
+
     for (let k = j + 1; k < N; k++) {
-      let maxSum = editedInput[i] + editedInput[j] + editedInput[k];
-      
-      if (maxSum <= M) {
-        maxSumArr.push(maxSum);
+      let thirdNum = cards[k];
+      let sumCandidate = firstNum + secondNum + thirdNum;
+
+      if (sum < sumCandidate && sumCandidate <= M) {
+        sum = sumCandidate;
       }
     }
   }
 }
 
-console.log(Math.max(...maxSumArr));
+console.log(sum);
