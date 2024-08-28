c2674a337c2b5f70b80df95806ba819e6798bd71
Dongbin Yoo <dongbin420@gmail.com>
2024-08-28 20:30:03 +0900

    [Silver III] Title: 소수 구하기, Time: 188 ms, Memory: 25068 KB -BaekjoonHub

--- 백준/Silver/1929. 소수 구하기/소수 구하기.js
+++ 백준/Silver/1929. 소수 구하기/소수 구하기.js
@@ -1,27 +1,42 @@
 const fs = require('fs');
 const filePath = process.platform === 'linux' ? '/dev/stdin' : '../../input.txt';
-let input = fs.readFileSync(filePath).toString().split(' ').map(line => Number(line.replace(/\r/g, '')));
+let input = fs
+  .readFileSync(filePath)
+  .toString()
+  .split(' ')
+  .map((line) => Number(line.replace(/\r/g, '')));
 
-const M = input[0];
-const N = input[1];
+const [M, N] = input;
+const primeNums = Array(N + 1).fill(true);
+primeNums[0] = primeNums[1] = false;
+let answer = '';
 
-const isPrime = new Array(N + 1).fill(true);
-isPrime[0] = isPrime[1] = false;
+// for (let i = 2; i <= Math.sqrt(N); i++) {
+//   for (let j = i * i; j <= N; j += i) {
+//     if (primeNums[j]) {
+//       primeNums[j] = false;
+//     }
+//   }
+// }
 
 for (let i = 2; i <= Math.sqrt(N); i++) {
-  if (isPrime[i]) {
+  if (primeNums[i]) {
     for (let j = i * i; j <= N; j += i) {
-      isPrime[j] = false;
+      primeNums[j] = false;
     }
   }
 }
 
-const answer = [];
-
 for (let k = M; k <= N; k++) {
-  if (isPrime[k]) {
-    answer.push(k);
+  if (primeNums[k]) {
+    answer += `${k}\n`;
   }
 }
 
-console.log(answer.join('\n'));
+// for (const idx in primeNums) {
+//   if (idx >= M && primeNums[idx]) {
+//     answer += `${idx}\n`;
+//   }
+// }
+
+console.log(answer.trim());
