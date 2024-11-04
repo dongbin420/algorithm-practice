61bd7e07ab25af40f6dff490d869e25be53ae3ff
Dongbin Yoo <dongbin420@gmail.com>
2024-11-04 16:53:41 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 34.34 ms, Memory: 64.6 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.js
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.js
@@ -1,6 +1,7 @@
 function solution(participant, completion) {
     const mapOfParticipant = new Map();
     
+    // 한 사람의 키가 이미 존재하면, 기존 값에서 1더하기, 존재하지않으면, 1로 초기화
     participant.forEach((person) => {
         if (mapOfParticipant.get(person)) {
             mapOfParticipant.set(person, mapOfParticipant.get(person) + 1);
@@ -9,10 +10,12 @@ function solution(participant, completion) {
         }
     })
     
+    // 완주자에 해당할때마다, 해당 키 값에 -1.
     completion.forEach((person) => {
         mapOfParticipant.set(person, mapOfParticipant.get(person) - 1);
     })
     
+    // 키 값이 0이 아닌 경우를 동명이인이 있을때와 없을때 두 경우로 생각할 수 있음. 동명이인이 없는 경우는 완주하지 못한 사람만 키값이 0이 아닐것이고, 동명이인이 있는 경우도 동명이인 2명 중 한명만 완주했을때 키값이 1일 것이기에 0이 아닌 사람만 체크해서 리턴
     for ([key, value] of mapOfParticipant) {
         if (value !== 0) {
             return key;
