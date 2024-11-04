96dbdae03fe2784414a7bdbd6c7ec4768f03dcf6
Dongbin Yoo <dongbin420@gmail.com>
2024-11-04 18:01:34 +0900

    [level 2] Title: 전화번호 목록, Time: 274.01 ms, Memory: 102 MB -BaekjoonHub

---
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.js
@@ -0,0 +1,84 @@
+function solution(phone_book) {
+    const setOfPhoneBook = new Set(phone_book);
+    
+    for (let i = 0; i < phone_book.length; i++) {
+        for (let j = 1; j < phone_book[i].length; j++) {
+            const startNum = phone_book[i].slice(0, j);
+            
+            if (setOfPhoneBook.has(startNum)) {
+                return false;
+            }
+        }
+    }
+    
+    return true
+}
+// 마지막으로 푼 방식 설명
+// 모든 폰 넘버를 돌면서, 각 폰 넘버를 1자리부터 전체 넘버까지 잘라서
+// 각 자른 넘버들이 phone book에 존재하는지 체크하는 방식
+// 예를 들어, 119에서 1, 11, 119로 잘라내면서 
+// 1이 phone book에 존재하는지, 11이 존재하는지, 119가 존재하는지를 체크
+// 사실, 맨 아래의 맨 처음으로 푼 방식과 기본 아이디어는 같음
+// 어떻게 더 효율적인 방식을 생각하냐의 차이
+
+
+// 세번째 푼 방식, phone_book 배열의 길이가 n일때, 시간 복잡도는 n^2. 여전히 시간 복잡도에서 시간초과가 뜨는 풀이
+// function solution(phone_book) {
+//     for (let i = 0; i < phone_book.length; i++) {
+//         for (let j = 0; j < phone_book.length; j++) {
+//             if (j !== i) {
+//                 const editedNum = phone_book[j].slice(0, phone_book[i].length);
+//                 if (editedNum === phone_book[i]) {
+//                     return false;
+//                 }
+//             }
+//         }
+//     }
+    
+//     return true;
+// }
+
+
+// 두번째로 푼 방식으로, n이 phone_book 배열의 길이, m이 각 번호의 길이라고 했을때, 시간복잡도가(n^2 * m^2)로 매우 높게 나옴. 문제를 푸는 로직은 맞지만, 시간 효율성이 매우 떨어지는 방법
+// function solution(phone_book) {
+//     for (let i = 0; i < phone_book.length; i++) {
+//         const mapOfNums = new Map();
+        
+//         for (let j = 0; j < phone_book[i].length; j++) {
+//             mapOfNums.set(j, phone_book[i].slice(0, j + 1));
+//         }
+        
+//         for (const [key, value] of mapOfNums) {
+//             for (let k = 0; k < phone_book.length; k++) {
+//                 if (k !== i && phone_book[k] === value) {
+//                     return false;
+//                 }
+//             }
+//         }
+//     }
+    
+//     return true;
+// }
+
+
+// 맨 처음에 푼 방식으로, forEach의 작동 방식을 몰라서 리턴을 잘못함.
+// function solution(phone_book) {
+//     for (let i = 0; i < phone_book.length; i++) {
+//         const mapOfNums = new Map();
+        
+//         for (let j = 0; j < phone_book[i].length; j++) {
+//             mapOfNums.set(j, phone_book[i].slice(0, j + 1));
+//         }
+        
+//         for (const [key, value] of mapOfNums) {
+//             phone_book.forEach((number, idx) => {
+//                 if (idx !== i && number === value) {
+//                     // 이 리턴은 solution 함수를 정상적으로 리턴하지 않는다. 왜냐하면 forEach내에서 쓰인 리턴은 forEach의 반복만 중단할 뿐 solution 함수에는 영향을 끼치지 않음. 일반 for 문을 써야함
+//                     return false;
+//                 }
+//             })
+//         }
+//     }
+    
+//     return true;
+// }
