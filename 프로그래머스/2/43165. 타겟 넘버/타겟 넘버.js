b6be14f4052013adde19e2a14b50584399574f89
Dongbin Yoo <dongbin420@gmail.com>
2024-11-21 04:04:18 +0900

    [level 2] Title: 타겟 넘버, Time: 269.98 ms, Memory: 36.1 MB -BaekjoonHub

---
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.js
@@ -0,0 +1,72 @@
+// dfs 방식(재귀)
+function solution(numbers, target) {
+    let cases = 0;
+    
+    const dfs = (idx, curSum) => {
+        if (idx === numbers.length) {
+            if (curSum === target) {
+                cases++;
+            }
+            
+            return;
+        }
+        
+        dfs(idx + 1, curSum + numbers[idx]);
+        dfs(idx + 1, curSum - numbers[idx]);
+    }
+    
+    dfs(0, 0);
+    
+    return cases;
+}
+
+// DFS방식(스택)
+// function solution(numbers, target) {
+//     let cases = 0;
+//     const stack = [[0, 0]];
+    
+//     while (stack.length > 0) {
+//         const [idx, curSum] = stack.pop();
+        
+//         if (idx === numbers.length) {
+//             if (curSum === target) {
+//                 cases++;
+//             }
+            
+//             continue;
+//         }
+        
+//         stack.push([idx + 1, curSum + numbers[idx]]);
+//         stack.push([idx + 1, curSum - numbers[idx]]);
+//     }
+    
+//     return cases;
+// }
+
+
+// BFS 방식
+// function solution(numbers, target) {
+//     let cases = 0;
+//     const queue = [[0, 0]];
+    
+//     while (queue.length > 0) {
+//         const [idx, curSum] = queue.shift();
+        
+//         if (idx === numbers.length) {
+//             if (curSum === target) {
+//                 cases++;
+//             }
+            
+//             continue;
+//         }
+        
+//         queue.push([idx + 1, curSum + numbers[idx]]);
+//         queue.push([idx + 1, curSum - numbers[idx]]);
+//     }
+    
+//     return cases;
+// }
+
+
+
+
