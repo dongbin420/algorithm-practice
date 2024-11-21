9211211333e43df1cea9a651e47b166afadac2bf
Dongbin Yoo <dongbin420@gmail.com>
2024-11-21 21:17:37 +0900

    [level 3] Title: 네트워크, Time: 0.37 ms, Memory: 33.7 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.js
+++ 프로그래머스/3/43162. 네트워크/네트워크.js
@@ -1,19 +1,19 @@
-// dfs(스택 방식)
+// bfs
 function solution(n, computers) {
     const visited = new Array(n).fill(false);
     let networks = 0;
     
-    const dfs = (startNode) => {
-        const stack = [startNode];
+    const bfs = (startNode) => {
+        const queue = [startNode];
         visited[startNode] = true;
         
-        while (stack.length > 0) {
-            const node = stack.pop();
+        while (queue.length > 0) {
+            const node = queue.shift();
             
             for (let i = 0; i < n; i++) {
-                if (computers[node][i] === 1 && !visited[i]) {
+                if (!visited[i] && computers[node][i] === 1) {
                     visited[i] = true;
-                    stack.push(i);
+                    queue.push(i);
                 }
             }
         }
@@ -22,9 +22,65 @@ function solution(n, computers) {
     for (let i = 0; i < n; i++) {
         if (!visited[i]) {
             networks++;
-            dfs(i);
+            bfs(i);
         }
     }
     
-    return networks
+    return networks;
 }
+
+// dfs(재귀 방식)
+// function solution(n, computers) {
+//     const visited = new Array(n).fill(false);
+//     let networks = 0;
+    
+//     const dfs = (node) => {
+//         visited[node] = true;
+        
+//         for (let i = 0; i < n; i++) {
+//             if (!visited[i] && computers[node][i] === 1) {
+//                 dfs(i);
+//             }
+//         }
+//     }
+    
+//     for (let i = 0; i < n; i++) {
+//         if (!visited[i]) {
+//             networks++;
+//             dfs(i);
+//         }
+//     }
+    
+//     return networks;
+// }
+
+// dfs(스택 방식)
+// function solution(n, computers) {
+//     const visited = new Array(n).fill(false);
+//     let networks = 0;
+    
+//     const dfs = (startNode) => {
+//         const stack = [startNode];
+//         visited[startNode] = true;
+        
+//         while (stack.length > 0) {
+//             const node = stack.pop();
+            
+//             for (let i = 0; i < n; i++) {
+//                 if (computers[node][i] === 1 && !visited[i]) {
+//                     visited[i] = true;
+//                     stack.push(i);
+//                 }
+//             }
+//         }
+//     }
+    
+//     for (let i = 0; i < n; i++) {
+//         if (!visited[i]) {
+//             networks++;
+//             dfs(i);
+//         }
+//     }
+    
+//     return networks
+// }
