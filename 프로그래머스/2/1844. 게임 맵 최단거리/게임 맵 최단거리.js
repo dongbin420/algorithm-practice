6cb254f7fc99aca42b0cb3dd6bff56acb9d05722
Dongbin Yoo <dongbin420@gmail.com>
2024-11-25 17:39:31 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 25.95 ms, Memory: 39 MB -BaekjoonHub

---
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.js
@@ -0,0 +1,31 @@
+// 최단 경로 문제이므로, bfs가 적절함.
+function solution(maps) {
+    const row = maps.length;
+    const col = maps[0].length;
+    const visited = Array.from({ length: row }, () => Array(col).fill(false));
+    const directions = [
+        [0, 1], [0, -1], [1, 0], [-1, 0]
+    ];
+    const queue = [[0, 0, 1]];
+    
+    while (queue.length > 0) {
+        const [x, y, dist] = queue.shift();
+        visited[x][y] = true;
+        
+        if (x === row - 1 && y === col - 1) {
+            return dist;
+        }
+        
+        for (const [dx, dy] of directions) {
+            const nx = x + dx;
+            const ny = y + dy;
+            
+            if (nx >= 0 && nx < row && ny >= 0 && ny < col && !visited[nx][ny] && maps[nx][ny] === 1) {
+                queue.push([nx, ny, dist + 1]);
+                visited[nx][ny] = true;
+            }
+        }
+    }
+    
+    return -1;
+}
