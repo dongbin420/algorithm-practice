6f63daf6557592d18a1c3ac6773d900793eab824
Dongbin Yoo <dongbin420@gmail.com>
2024-11-27 17:00:10 +0900

    [level 3] Title: 단어 변환, Time: 0.60 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/3/43163. 단어 변환/단어 변환.js
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.js
@@ -1,11 +1,5 @@
-// bfs를 활용한 첫번째 풀이
 function solution(begin, target, words) {
-    const visited = {}
-    
-    for (const word of words) {
-        visited[word] = false;
-    }
-    
+    const visited = [];
     const queue = [[begin, 0]];
     
     while (queue.length > 0) {
@@ -15,15 +9,60 @@ function solution(begin, target, words) {
             return steps;
         }
         
-        for (let i = 0; i < words.length; i++) {
-            const differences = words[i].split('').filter((char, idx) => char !== deletedWord[idx]);
+        for (const word of words) {
+            const isChangeable = deletedWord.split('').filter((char, idx) => word[idx] !== char).length === 1 ? true : false;
             
-            if (differences.length === 1 && !visited[words[i]]) {
-                visited[words[i]] = true;
-                queue.push([words[i], steps + 1]);
+            if (isChangeable && !visited.includes(word)) {
+                queue.push([word, steps + 1]);
+                visited.push(word);
             }
         }
     }
     
     return 0;
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// bfs를 활용한 첫번째 풀이
+// function solution(begin, target, words) {
+//     const visited = {}
+    
+//     for (const word of words) {
+//         visited[word] = false;
+//     }
+    
+//     const queue = [[begin, 0]];
+    
+//     while (queue.length > 0) {
+//         const [deletedWord, steps] = queue.shift();
+        
+//         if (deletedWord === target) {
+//             return steps;
+//         }
+        
+//         for (let i = 0; i < words.length; i++) {
+//             const differences = words[i].split('').filter((char, idx) => char !== deletedWord[idx]);
+            
+//             if (differences.length === 1 && !visited[words[i]]) {
+//                 visited[words[i]] = true;
+//                 queue.push([words[i], steps + 1]);
+//             }
+//         }
+//     }
+    
+//     return 0;
+// }
