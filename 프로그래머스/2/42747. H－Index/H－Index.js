5e0105fa74af9cd8c60f00d83cd4c2ded6d32779
Dongbin Yoo <dongbin420@gmail.com>
2025-01-02 18:33:07 +0900

    [level 2] Title: H-Index, Time: 0.64 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/2/42747. H－Index/H－Index.js
@@ -0,0 +1,32 @@
+function solution(citations) {
+    citations.sort((a, b) => b - a);
+    
+    for (let i = 0; i < citations.length; i++) {
+        if (citations[i] < i + 1) {
+            return i;
+        }
+    }
+    
+    return citations.length;
+}
+
+
+// 처음에 푼 방식(아마도, 시간 복잡도에서 만족하지 않는 것으로 보임)
+// function solution(citations) {
+//     citations.sort((a, b) => b - a);
+    
+//     for (let i = 0; i < citations.length; i++) {
+//         const h = citations[i];
+//         let cnt = 0;
+        
+//         for (let j = 0; j < citations.length; j++) {
+//             if (citations[j] >= h) {
+//                 cnt++;
+//             }
+//         }
+        
+//         if (cnt >= h) {
+//             return h;
+//         }
+//     }
+// }
