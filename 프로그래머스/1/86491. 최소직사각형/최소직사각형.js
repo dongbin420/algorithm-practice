66718a5e19a15a91ca5c9ac3df2c6862b4f0cfbd
Dongbin Yoo <dongbin420@gmail.com>
2025-01-06 19:34:38 +0900

    [level 1] Title: 최소직사각형, Time: 2.47 ms, Memory: 38.7 MB -BaekjoonHub

--- 프로그래머스/1/86491. 최소직사각형/최소직사각형.js
+++ 프로그래머스/1/86491. 최소직사각형/최소직사각형.js
@@ -1,27 +1,36 @@
 function solution(sizes) {
-    const horizontal = []
-    const vertical = [];
+    let horizontalMax = 0;
+    let verticalMax = 0;
     
     for (let i = 0; i < sizes.length; i++) {
-        horizontal.push(Math.max(sizes[i][0], sizes[i][1]));
-        vertical.push(Math.min(sizes[i][1], sizes[i][0]));
+        const bigger = Math.max(sizes[i][0], sizes[i][1]);
+        const smaller = Math.min(sizes[i][0], sizes[i][1]);
+        
+        if (bigger > horizontalMax) {
+            horizontalMax = bigger;
+        }
+        
+        if (smaller > verticalMax) {
+            verticalMax = smaller;
+        }
     }
     
-    return Math.max(...horizontal) * Math.max(...vertical);
+    return horizontalMax * verticalMax;
 }
 
+// function solution(sizes) {
+//     const horizontal = []
+//     const vertical = [];
+    
+//     for (let i = 0; i < sizes.length; i++) {
+//         horizontal.push(Math.max(sizes[i][0], sizes[i][1]));
+//         vertical.push(Math.min(sizes[i][1], sizes[i][0]));
+//     }
+    
+//     return Math.max(...horizontal) * Math.max(...vertical);
+// }
 
-
-
-
-
-
-
-
-
-
-
-
+// 쉽게 생각해서, 모든 명함의 긴변 중 가장 긴 크기와 모든 명함의 짧은 면 중 가장 긴 크기를 조합해야 모든 명함을 다 수용하면서도 가장 작은 크기의 지갑이 완성된다. 
 
 // 오래전에 푼 방식
 // function solution(sizes) {
