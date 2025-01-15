ad4143c263a063e91ddd241f03888f7c13ae8489
Dongbin Yoo <dongbin420@gmail.com>
2025-01-15 17:26:46 +0900

    [level 2] Title: 전력망을 둘로 나누기, Time: 16.71 ms, Memory: 37.2 MB -BaekjoonHub

--- 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.js
+++ 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.js
@@ -16,11 +16,11 @@ function solution(n, wires) {
         tree[end][start] = 0;
         
         // 두 개로 나뉜 전력망의 송전탑 개수 각각 구하고, 차이 구하기
-        // 두 개로 나뉜 트리를 각각 bfs로 순회하면서 전선의 개수를 구하고 차이 구하기
+        // 두 개로 나뉜 트리를 각각 bfs로 순회하면서 송전의 개수를 구하고 차이 구하기
         const visited = new Array(n + 1).fill(false);
         
         const bfs = (startNode) => {
-            let lineCnt = 0;
+            let cnt = 1;
             const queue = [startNode];
             visited[startNode] = true;
             
@@ -31,23 +31,17 @@ function solution(n, wires) {
                     if (!visited[i] && tree[node][i] === 1) {
                         visited[i] = true;
                         queue.push(i);
-                        lineCnt++;
+                        cnt++;
                     }
                 }
             }
             
-            return lineCnt;
+            return cnt;
         }
         
-        const lines = []
-        
-        for (let i = 1; i < n + 1; i++) {
-            if (!visited[i]) {
-                lines.push(bfs(i));
-            }
-        }
-        
-        diffs.push(Math.abs(lines[0] - lines[1]));
+        // 아무데서나 bfs를 한번만 실시하고, 이때 노드의 수를 이용해서 다른 전력망의 노드 수를 구해서 차이를 구하는 간단한 방식
+        const cnt = bfs(start)
+        diffs.push(Math.abs(cnt - (n - cnt)));
         
         // 원상 복구
         tree[start][end] = 1;
