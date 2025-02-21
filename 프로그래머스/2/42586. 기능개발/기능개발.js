b9e0b5cff2c7fe02879443871a867fa5c096eecc
Dongbin Yoo <dongbin420@gmail.com>
2025-02-21 15:29:54 +0900

    [level 2] Title: 기능개발, Time: 0.71 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.js
+++ 프로그래머스/2/42586. 기능개발/기능개발.js
@@ -1,30 +1,30 @@
 // 다른 방식 (효율성을 개선한 방식)(일반적인 스택/큐 방식(여기선 큐)을 이용한)
-function solution(progresses, speeds) {
-    const days = [];
+// function solution(progresses, speeds) {
+//     const days = [];
     
-    for (let i = 0; i < progresses.length; i++) {
-        const day = Math.ceil((100 - progresses[i]) / speeds[i]);
-        days.push(day);
-    }
+//     for (let i = 0; i < progresses.length; i++) {
+//         const day = Math.ceil((100 - progresses[i]) / speeds[i]);
+//         days.push(day);
+//     }
     
-    const answer = [];
-    let standard = days[0];
-    let cnt = 1;
+//     const answer = [];
+//     let standard = days[0];
+//     let cnt = 1;
     
-    for (let i = 1; i < days.length; i++) {
-        if (days[i] > standard) {
-            answer.push(cnt);
-            standard = days[i];
-            cnt = 1;
-        } else {
-            cnt++;
-        }
-    }
+//     for (let i = 1; i < days.length; i++) {
+//         if (days[i] > standard) {
+//             answer.push(cnt);
+//             standard = days[i];
+//             cnt = 1;
+//         } else {
+//             cnt++;
+//         }
+//     }
     
-    answer.push(cnt);
+//     answer.push(cnt);
     
-    return answer;
-}
+//     return answer;
+// }
 
 // 처음 푼 방식(조금 비효율적일 수 있음)
 // function solution(progresses, speeds) {
@@ -54,3 +54,30 @@ function solution(progresses, speeds) {
     
 //     return answer;
 // }
+
+
+function solution(progresses, speeds) {
+    var answer = [];
+
+    while(speeds.length > 0) {
+        // 개발
+        for(let i in speeds) {
+            if(progresses[i] < 100) {
+                progresses[i] += speeds[i];
+            }
+        }
+
+        // 배포
+        let deploy_count = 0;
+        while(progresses[0] >= 100) {
+            progresses.shift();
+            speeds.shift();
+            deploy_count++;
+        }
+        if(deploy_count > 0) {
+            answer.push(deploy_count);
+        }
+    }
+
+    return answer;
+}
