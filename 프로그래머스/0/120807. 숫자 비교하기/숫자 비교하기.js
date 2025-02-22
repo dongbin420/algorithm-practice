7777b51f464e9553e0c801cf5a9e303a0910ddd7
Dongbin Yoo <dongbin420@gmail.com>
2025-02-22 15:54:13 +0900

    [level 0] Title: 숫자 비교하기, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/120807. 숫자 비교하기/숫자 비교하기.js
@@ -0,0 +1,14 @@
+function solution(num1, num2) {
+    if (num1 === num2) return 1;
+    
+    if (num1 !== num2) return -1;
+    
+    
+    
+    // if(num1 === num2) {
+    //     var answer = 1;
+    // } else if(num1 !== num2) {
+    //     var answer = -1;
+    // }
+    // return answer;
+}
