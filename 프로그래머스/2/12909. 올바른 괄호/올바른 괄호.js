cb697cd208989bc0e90c02a914900ce49ac6c8f6
Dongbin Yoo <dongbin420@gmail.com>
2025-02-24 18:45:07 +0900

    [level 2] Title: 올바른 괄호, Time: 3.96 ms, Memory: 37.6 MB -BaekjoonHub

---
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.js
@@ -0,0 +1,21 @@
+function solution(s){
+    const stack = [];
+    
+    for (let i = 0; i < s.length; i++) {
+        if (s[i] === '(') {
+            stack.push('(');
+        } else {
+            if (stack.length > 0) {
+                stack.pop();
+            } else {
+                return false;
+            }
+        }
+    }
+    
+    if (stack.length > 0) {
+        return false;
+    } else {
+        return true;
+    }
+}
