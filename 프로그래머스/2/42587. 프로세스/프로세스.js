be677ef5f95bf70dfb5b8d970c394a55dc4ed246
Dongbin Yoo <dongbin420@gmail.com>
2025-02-25 17:53:10 +0900

    [level 2] Title: 프로세스, Time: 1.26 ms, Memory: 33.9 MB -BaekjoonHub

--- 프로그래머스/2/42587. 프로세스/프로세스.js
+++ 프로그래머스/2/42587. 프로세스/프로세스.js
@@ -1,25 +1,48 @@
+// 아래 제출 코드 조금 개선한 코드
 function solution(priorities, location) {
-    const prioritiesAndIdx = priorities.map((prior, i) => ({ prior, idx: i }));
-    let orderCnt = 0;
-    
-    while (prioritiesAndIdx.length > 0) {
-        const maxPriority = Math.max(...prioritiesAndIdx.map((ele) => ele.prior));
-        
-        if (prioritiesAndIdx[0].prior === maxPriority) {
-            const deleted = prioritiesAndIdx.shift();
-            orderCnt++;
-            
-            if (deleted.idx === location) {
-                return orderCnt;
-            }
-        } else {
-            prioritiesAndIdx.push(prioritiesAndIdx.shift());
-        }
+  const priorAndTarget = priorities.map((prior, idx) => {
+    return { prior, target: idx === location };
+  })
+  let orderCnt = 0;
+
+  while (priorAndTarget.length > 0) {
+    const maxPrior = Math.max(...priorAndTarget.map((ele) => ele.prior));
+    const executedProcess = priorAndTarget.shift();
+
+    if (executedProcess.prior === maxPrior) {
+      orderCnt++;
+
+      if (executedProcess.target) return orderCnt;
+    } else {
+      priorAndTarget.push(executedProcess);
     }
+  }
+}
+
+// 제출 코드(정답)
+// function solution(priorities, location) {
+//     const prioritiesAndIdx = priorities.map((prior, i) => ({ prior, idx: i }));
+//     let orderCnt = 0;
+    
+//     while (prioritiesAndIdx.length > 0) {
+//         const maxPriority = Math.max(...prioritiesAndIdx.map((ele) => ele.prior));
+        
+//         if (prioritiesAndIdx[0].prior === maxPriority) {
+//             const deleted = prioritiesAndIdx.shift();
+//             orderCnt++;
+            
+//             if (deleted.idx === location) {
+//                 return orderCnt;
+//             }
+//         } else {
+//             prioritiesAndIdx.push(prioritiesAndIdx.shift());
+//         }
+//     }
+// }
     
     
-    
-// 이전 시도1
+// 이전 시도1(틀린 코드)
+// function solution(priorities, location) {
 //     const prioritiesAndIdx = [];
 //     let orderCnt = 0;
 //     priorities.forEach((prior, i) => prioritiesAndIdx.push({prior: prior, idx: i, order: null }));
@@ -34,12 +57,11 @@ function solution(priorities, location) {
 //     }
     
 //     return prioritiesAndIdx[location].order;
-    
-    
-    
-    
-    
-// 이전 시도2
+// }
+
+  
+// 이전 시도2(틀린 코드)
+// function solution(priorities, location) {
 //     const order = Array(priorities.length);
 //     let orderCnt = 0;
 //     let orderNum = 0;
@@ -54,4 +76,4 @@ function solution(priorities, location) {
 //     }
     
 //     return order[location];
-}
+// }
