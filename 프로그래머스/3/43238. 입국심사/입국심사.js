76ee6ab89fdb9e9ee79f42923e3237aad7021042
Dongbin Yoo <dongbin420@gmail.com>
2025-02-28 22:23:00 +0900

    [level 3] Title: 입국심사, Time: 120.78 ms, Memory: 42.4 MB -BaekjoonHub

---
+++ 프로그래머스/3/43238. 입국심사/입국심사.js
@@ -0,0 +1,70 @@
+function solution(n, times) {
+  let left = 1;
+  let right = n * Math.max(...times);
+
+  while (left <= right) {
+    const mid = Math.floor((left + right) / 2);
+    let possiblePeople = 0;
+
+    times.forEach((time) => {
+      possiblePeople += Math.floor(mid / time);
+    });
+
+    if (possiblePeople >= n) {
+      right = mid - 1;
+    } else {
+      left = mid + 1;
+    }
+  }
+
+  return left;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// 오래 전에 풀었던 방식
+// function solution(n, times) {
+    
+//     // max_1 = 심사관 중에 가장 오래 걸리는 시간
+//     // 오름 차순 정렬
+//     times.sort((a, b) => a - b);
+    
+//     let minTime = times[0];
+//     let maxTime = times[times.length - 1] * n;
+    
+//     let answer = maxTime;
+    
+//     while(minTime <= maxTime) {
+//         let midTime = Math.floor((minTime+maxTime)/2);
+        
+//         let count = 0;
+        
+//         for (let time of times) {
+//             count += Math.floor(midTime/time);
+//         }
+        
+//         if (count >= n) {
+            
+//             answer = midTime;
+//             // 조건을 충족한 경우
+//             maxTime = midTime - 1;
+//         } else {
+//             // 조건을 충족 하지 못한 경우
+//             minTime = midTime + 1;
+//         }
+//     }
+    
+//     return answer;
+// }
