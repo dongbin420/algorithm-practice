eaaadac2edce57374e577b47500c5621d963985b
Dongbin Yoo <dongbin420@gmail.com>
2025-04-25 20:02:36 +0900

    [level 3] Title: N으로 표현, Time: 19.88 ms, Memory: 39.2 MB -BaekjoonHub

--- 프로그래머스/3/42895. N으로 표현/N으로 표현.js
+++ 프로그래머스/3/42895. N으로 표현/N으로 표현.js
@@ -23,6 +23,18 @@ function solution(N, number) {
   return -1;
 }
 
+// 위 코드의 동작 방식
+// i가 1일땐, 첫번째 for문만 실행되고 오직 한개 N만 dp에 더해짐
+// i가 2일땐, 첫번째 for문으로 NN이 dp에 더해지고 두번째 for문은
+// 한 번만 실행됨. dp[1]의 1개 N을 가지고 N을 두 개 쓰는 모든 경우의 수를
+// dp[2]에 추가함. 예를 들어, N + N, N - N, N / N, N * N
+// i가 3일땐, 첫번째 for문으로 NNN이 dp에 더해지고 세번째 for문은
+// 두번 실행됨. dp[1]과 dp[2]의 요소들의 모든 사칙연산 조합과
+// dp[2]와 dp[1](반대 방향 사칙연산도 고려해야하니)의 요소들의
+// 모든 사칙연산 조합이 dp[3]에 저장됨. 이렇게 이전 dp요소들을
+// 이용해서 다음의 dp요소들을 만들어내어 dp방식으로 문제를 푸는 것.
+
+
 // 첫번째 시도(틀린 코드)
 // function solution(N, number) {
 //     const dp = Array.from({ length: 9 }, () => new Set());
